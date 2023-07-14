44d0a3f78840293bf2318e4643b36b72e6d35704
Krittiya Peung <krittiya.s@20scoops.net>
2023-07-14 11:33:50 +0700

    add starter files

---
+++ src/users/users.controller.ts
@@ -0,0 +1,46 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CreateUserDto, UpdateUserDto } from '../dto/user.dto';
+import { UsersService } from './users.service';
+import { User } from 'src/interfaces/user.interface';
+
+@Controller('users')
+export class UsersController {
+    constructor(private usersService: UsersService){}
+
+    // Post Method Simple Example
+    // @Post()
+    // create(@Body() createCatDto: CreateCatDto) {
+    //     return 'This action adds a new cat';
+    //   }
+    @Post()
+    async create(@Body() createUserDto: CreateUserDto): Promise<CreateUserDto> {
+        this.usersService.create(createUserDto);
+        return createUserDto;
+    }
+
+    // Get Method Simple Example
+    // @Get()
+    // findAll(@Req() request: Request): string{
+    //     return 'List of users';
+    // }
+    @Get()
+    async findAll(): Promise<User[]>{
+        return this.usersService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param() params: any): string{
+        console.log(params.id);
+        return `This action returns a #${params.id} user`;
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto){
+        return `This action updates a #${id} user`;
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return `This action removes a #${id} user`;
+    }
+}
