4f8b6b9111150a9d53c7ac2fcc5f6f9f728ecd1a
Krittiya Peung <krittiya.s@20scoops.net>
2023-07-17 15:25:19 +0700

    [feature] add user validators

--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -1,5 +1,13 @@
+import { UseSchema } from 'nestjs-yup';
+import { UsersValidator } from '../users/users.validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+// TODO: error here
+// @UseSchema(UsersValidator)
 export class CreateUserDto {
+  @ApiProperty()
   name: string;
+  @ApiProperty()
   age: number;
 }
 
---
+++ src/users/users.validator.ts
@@ -0,0 +1,6 @@
+import yup from 'yup';
+
+export const UsersValidator = yup.object({
+    name: yup.string().required().min(3).max(10),
+    age: yup.number().required().min(1).required(),
+  });
