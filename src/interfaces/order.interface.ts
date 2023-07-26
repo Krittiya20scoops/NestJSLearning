b1d76538ef516ba7348633025e047066068b2369
Krittiya Peung <krittiya.s@20scoops.net>
2023-07-26 15:23:49 +0700

    [chore] update order interface

--- src/interfaces/order.interface.ts
+++ src/interfaces/order.interface.ts
@@ -1,8 +1,10 @@
 import { Schema } from 'mongoose';
+import { User } from './user.interface';
+import { Product } from './product.interface';
 
 export interface Order {
-  user_id: Schema.Types.ObjectId;
-  product_id: Schema.Types.ObjectId;
+  user_id: Schema.Types.ObjectId | User;
+  product_id: Schema.Types.ObjectId | Product;
   quantity: number;
   total_price: number;
 }
