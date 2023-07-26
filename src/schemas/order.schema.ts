c3c5dfd8216435767b25f7a58da54ceada1deeaa
Krittiya Peung <krittiya.s@20scoops.net>
2023-07-26 15:19:43 +0700

    [chore] update order schema

--- src/schemas/order.schema.ts
+++ src/schemas/order.schema.ts
@@ -1,14 +1,16 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
+import { User } from './user.schema';
+import { Product } from './product.schema';
 
 export type OrderDocument = HydratedDocument<Order>;
 @Schema()
 export class Order {
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
-  user_id: mongoose.Schema.Types.ObjectId;
+  user_id: mongoose.Schema.Types.ObjectId | User;
 
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Product' })
-  product_id: mongoose.Schema.Types.ObjectId;
+  product_id: mongoose.Schema.Types.ObjectId | Product;
 
   @Prop()
   quantity: number;
